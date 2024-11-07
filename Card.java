c6e6a8f4c1df13746cd4fb6928b2ed7d2070a1e1
Ahnaf-BD <147507788+Ahnaf-BD@users.noreply.github.com>
2024-11-07 18:58:50 +0000

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,1 @@
+public class Card { private final int value; public Card(int value) { this.value = value; } public int getValue() { return this.value; } @Override public String toString() { return String.valueOf(this.value); } }
